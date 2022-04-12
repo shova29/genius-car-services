977fb593bea5a5a8bbb18ba390d54de970ae2f2a
shova29 <halima2996@gmail.com>
2022-04-13 02:58:36 +0600

    navbar,banner and our experts component added in the homepage

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -10,12 +10,16 @@ const Services = () => {
       .then((data) => setServices(data));
   }, []);
   return (
-    <div>
-      <h2 className="services-title">Our Services</h2>
-      <div className="services-container">
-        {services.map((service) => (
-          <Service key={service.id} service={service}></Service>
-        ))}
+    <div className="container">
+      <div className="row">
+        <h2 className="services-title text-center text-primary mt-5">
+          Our Services
+        </h2>
+        <div className="services-container mt-5">
+          {services.map((service) => (
+            <Service key={service.id} service={service}></Service>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,10 +1,13 @@
 import React from "react";
 
 const Footer = () => {
+  const today = new Date();
+  const year = today.getFullYear();
   return (
-    <footer>
+    <footer className="text-center mt-5">
       <p>
-        <small> &copy;copyright &#64;{new Date().getFullYear()}</small>
+        {/*  <small> &copy;copyright &#64;{new Date().getFullYear()}</small> */}
+        <small> &copy;copyright &#64;{year}</small>
       </p>
     </footer>
   );
